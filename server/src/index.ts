9b748abd8b4b04598e0a7faa03babd7cf9cabb39
andreeeestor <andrenestor0503@gmail.com>
2025-10-25 14:05:59 -0300

    feat: inngest config implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,29 @@
-import express from "express"
+import express from "express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest";
+import { logger } from "./utils/logger";
 
 const app = express();
 const PORT = 3001;
 
-// parse JSON Body - Converte
-app.use(express.json())
+// parse JSON Body - Converte para poder usar os métodos POST
+app.use(express.json());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta: ${PORT}`);
-  console.log(`Acesse aqui: http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Olá mundo!");
 });
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      logger.info(`Servidor rodando na porta: ${PORT}`);
+      logger.info(`Acesse aqui: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    logger.error("Erro ao iniciar o servidor:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
